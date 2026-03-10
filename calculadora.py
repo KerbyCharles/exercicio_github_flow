ad5e8d593a8c9e56d49a0a7d13b7539cb17a4307
Kerby Charles <charleskerby16@gmail.com>
2026-03-10 13:57:40 -0300

    adicionar função de subtração

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,3 @@
 # Calculadora Básica
-# Desenvolvedor A: Implementar a função de soma aqui
-# Desenvolvedor B: Implementar a função de subtraçã aqui
+# Calculadora Básica
+# Def subtracao(a, b): return a - b
